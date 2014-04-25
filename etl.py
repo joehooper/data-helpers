fa419909519764e4cebdd6a9ba3c6c198d1ed4b9
Joe Hooper <jhoops51@gmail.com>
2014-04-25 18:07:35 -0400

    Initial commit

---
+++ etl.py
@@ -0,0 +1,35 @@
+###
+# Data munging functions for sports data
+###
+
+import re
+
+
+def get_inches(ft_in):
+    # get inches from str formatted ft' in" as float
+    ft_in = str(ft_in)
+    inches_re = re.compile(r'[0-9]\' (.*)\"')
+    if re.search(inches_re, ft_in):
+        return float(re.search(inches_re, ft_in).groups(0)[0].strip())
+    else:
+        return 0.0
+
+def get_feet(ft_in):
+    # get feet from str formatted ft' in" as int
+    ft_in = str(ft_in)
+    feet_re = re.compile(r'(^[0-9]{1,2})')
+    if re.search(feet_re, ft_in):
+        return int(re.search(feet_re, x).groups(0)[0])
+    else:
+        return 0
+
+def total_inches(str_series):
+    # takes a pandas Series of str formatted ft' in" i.e. 6' 8.5"
+    # returns float Series of total inches 
+    inches = str_series.apply(get_inches)
+    feet = str_series.apply(get_feet)
+    return feet*12 + inches
+    
+
+
+    
